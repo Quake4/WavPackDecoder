56d8271a2a0f5c3ef39126a502e1c2558bb587bb
Quake4 <batsov@yandex.ru>
2024-05-29 17:45:44 +0300

    test changes on github

--- WavPack/Bitstream.cs
+++ WavPack/Bitstream.cs
@@ -10,14 +10,14 @@
 
 namespace WavPack
 {
-	class Bitstream
-	{
-		internal int end, ptr; // was uchar in c
-		internal uint sr;
-		internal int file_bytes;
-		internal int error, bc;
-		internal System.IO.BinaryReader file;
-		internal byte[] buf = new byte[Defines.BITSTREAM_BUFFER_SIZE];
-		internal int buf_index = 0;
-	}
+class Bitstream
+{
+	internal int end, ptr; // was uchar in c
+	internal uint sr;
+	internal int file_bytes;
+	internal int error, bc;
+	internal System.IO.BinaryReader file;
+	internal byte[] buf = new byte[Defines.BITSTREAM_BUFFER_SIZE];
+	internal int buf_index = 0;
+}
 }
